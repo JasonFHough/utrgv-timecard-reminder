ca46e39b11a30843660d6c90a42680444160c559
Jason Hough <jasonhough0@gmail.com>
2020-10-25 02:08:29 -0400

    parses due dates from table

--- reminder.py
+++ reminder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup, element
 
 def get_html(url: str):
@@ -36,6 +37,52 @@ def get_monthly_table(soup: BeautifulSoup):
     table = soup.find_all("table", {"summary" : "Monthly Payroll Schedule"}, limit = 1)
     return table
 
+def get_semi_monthly_due_dates(table: element.ResultSet):
+    """
+    Return a list of all the due dates from the semi-monthly table
+
+    table - the semi_monthly_table BeautifulSoup ResultSet
+    """
+    if len(table) != 1:
+        return []
+    else:
+        # Find tag that resembles <strong>
+        strongs = table[0].find_all("strong")
+        due_dates = []
+        for strong in strongs:
+            strong_contents = strong.contents[0] # Should expect date to be in format: mm/dd/yyyy
+            try:
+                datetime_object = datetime.strptime(strong_contents, "%m/%d/%Y")
+                due_dates.append(datetime_object)
+            except:
+                # strong tag did not contain a date, so skip
+                continue
+
+        return due_dates
+
+def get_monthly_due_dates(table: element.ResultSet):
+    """
+    Return a list of all the due dates from the monthly table
+
+    table - the monthly_table BeautifulSoup ResultSet
+    """
+    if len(table) != 1:
+        return []
+    else:
+        # Find tag that resembles <td style="font-weight: bold; color: red;">
+        tds = table[0].find_all("td", {"style" : "font-weight: bold; color: red;"})
+        due_dates = []
+        for td in tds:
+            td_contents = td.contents[0] # Should expect date to be in format: mm/dd/yyyy
+            try:
+                datetime_object = datetime.strptime(td_contents, "%m/%d/%Y")
+                due_dates.append(datetime_object)
+            except:
+                # strong tag did not contain a date, so skip
+                continue
+
+        return due_dates
+
 def main():
     url = "https://www.utrgv.edu/financial-services-comptroller/departments/payroll-and-tax-compliance/payroll-schedules-and-deadlines/index.htm"
     html = get_html(url)
@@ -43,6 +90,9 @@ def main():
 
     semi_monthly_table = get_semi_monthly_table(soup)
     monthly_table = get_monthly_table(soup)
+
+    semi_monthly_due_dates = get_semi_monthly_due_dates(semi_monthly_table)
+    monthly_due_dates = get_monthly_due_dates(monthly_table)
 
 if __name__ == "__main__":
     main()
